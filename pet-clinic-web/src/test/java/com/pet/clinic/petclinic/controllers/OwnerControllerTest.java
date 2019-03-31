0d82b3e9ea85ec45e2e681a1aa986b29e1988f97
vladyslav-raikovskyi <vladyslav.raikovskyi@vitech.com.ua>
2019-03-31 10:49:35 +0300

    fix brocken test

--- pet-clinic-web/src/test/java/com/pet/clinic/petclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/pet/clinic/petclinic/controllers/OwnerControllerTest.java
@@ -16,6 +16,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -47,7 +48,7 @@ class OwnerControllerTest {
 
   @Test
   void listOfOwner() throws Exception {
-
+    when(ownerService.findAll()).thenReturn(owners);
     mockMvc.perform(get("/owners"))
         .andExpect(status().isOk())
         .andExpect(view().name("owners/index"))
